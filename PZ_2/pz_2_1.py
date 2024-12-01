8ffed582037a482b15f6814862511d59ee647985
MariaPogorelova <masha@pogoreloff.ru>
2024-12-01 11:22:56 +0300

    ПЗ_2_1 сделала

--- PZ_2/pz_2_1.py
+++ PZ_2/pz_2_1.py
@@ -1,1 +1,14 @@
-print('Hi')
+#Дано трехзначное число. Вывести число, полученное при перестановке цифр сотен и десятков исходного числа
+try:
+    number = int(input("Введите трёхзначное число: "))
+    str_number = str(number)
+
+    if len(str_number) == 3:
+        new_number = str_number[1] + str_number[0] + str_number[2]
+        result = int(new_number)
+        print(result)
+    else:
+        print("Ошибка: Введите трёхзначное число.")
+except Exception:
+    print("Ошибка: Ввод должен быть числом.")
+
