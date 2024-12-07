14c658fef6bd64ddd6fdb325e7e1140311082063
MariaPogorelova <masha@pogoreloff.ru>
2024-12-07 15:59:45 +0300

    выполнила дополнительное задание

---
+++ доп/№1.py
@@ -0,0 +1,8 @@
+a, b = float(input("Введите первое число: ")), float(input("Введите второе число: "))
+
+c = a * b
+if c < 0:
+    result = c * 8
+else:
+    result = c * 1.5
+print("Результат:", result)
---
+++ доп/№3.py
@@ -0,0 +1,19 @@
+number = input("Введите число: ")
+while True:
+    try:
+        number = int(number)
+        if 10 <= number <= 99:
+            break
+        else:
+            print("Ошибка: Число должно быть двухзначным.")
+            number = input("Введите двухзначное число: ")
+    except ValueError:
+        print("Ошибка: Ввод должен быть числом.")
+        number = input("Введите двузначное число: ")
+a = number // 10 #десятки
+b = number % 10 #единицы
+if (a + b) % 2 == 0:
+    result = number * 2
+else:
+    result = number / 2
+print("Результат:", result)
