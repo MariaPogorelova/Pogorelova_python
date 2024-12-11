f7f362cd8cb02197a9a5e58eaf9617aa4d9b8843
MariaPogorelova <masha@pogoreloff.ru>
2024-12-11 21:02:35 +0300

    ПЗ_4 сделала

--- PZ_4/pz_4_1.py
+++ PZ_4/pz_4_1.py
@@ -1,29 +1,41 @@
 #Дано вещественное число X и  целое число N (>0).
 #Найти значение выражения 1 - X^2/(2!) + X^4/(4!) - ..... + ((-1)^N - X^2N)/((2-N)!)/ N!
-try:
-    x = float(input("Введите значение X (вещественное число): "))
-    N = int(input("Введите значение N (целое число, N > 0): "))
+X = input("Введите значение X: ")
+while type(X) != float:
+    try:
+        X = float(X)
+    except ValueError:
+        print("Ошибка!")
+        X = input("Введите значение X (вещественное число): ")
 
-    if N <= 0:
-        print("Ошибка: N должно быть больше нуля")
-    else:
-        s = 0
-        sign = 1
-        n = 0
+N = input("Введите значение N: ")
+while type(N) != int:
+    try:
+        N = int(N)
+    except ValueError:
+        print("Ошибка!")
+        N = input("Введите значение N (целое число): ")
 
-        while n <= N:
-            factorial = 1
-            i = 1
-            while i <= 2 * n:
-                factorial *= i
-                i += 1
-            term = sign * (x ** (2 * n)) / factorial
-            s += term
-            sign *= -1
-            n += 1
-            result = s + 1
+if N <= 0:
+    print("Ошибка: N должно быть больше нуля")
 
-        print(f"Приближенное значение cos({x}): {result}")
-except ValueError:
-    print("Ошибка! Введите корректные числовые данные")
+else:
+    s = 0
+    sign = 1
+    n = 0
+
+    while n <= N:
+        factorial = 1
+        i = 1
+        while i <= 2 * n:
+            factorial *= i
+            i += 1
+        term = sign * (X ** (2 * n)) / factorial
+        s += term
+        sign *= -1
+        n += 1
+        result = s + 1
+
+    print(f"Приближенное значение cos({X}): {result}")
+
 
--- PZ_4/pz_4_2.py
+++ PZ_4/pz_4_2.py
@@ -1,18 +1,21 @@
 #Дано целое число N (>0)
 #Найти сумму 1^1 + 2^2 + N^N
 
-try:
-    N = int(input("Введите целое число: "))
+N = input("Введите целое число N: ")
+while type(N) != int:
+    try:
+        N = int(N)
+    except ValueError:
+        print("Ошибка!")
+        N = input("Введите целое число N: ")
 
-    if N <= 0:
-        print("Ошибка: N должно быть больше нуля")
-    else:
-        total_sum = 0
-        i = 1
-        while i <= N:
-            total_sum = total_sum + i ** i
-            i += 1
+if N <= 0:
+    print("Ошибка: N должно быть больше нуля")
+else:
+    total_sum = 0
+    i = 1
+    while i <= N:
+        total_sum = total_sum + i ** i
+        i += 1
 
-            print(total_sum)
-except ValueError:
-    print("Ошибка! Введите корректные числовые данные")
+    print(total_sum)
