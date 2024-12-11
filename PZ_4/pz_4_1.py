90edf75204f561bedcd531ddef128cf9480c2628
MariaPogorelova <masha@pogoreloff.ru>
2024-12-11 16:29:59 +0300

    добавила обработку исключения

--- PZ_4/pz_4_1.py
+++ PZ_4/pz_4_1.py
@@ -1,22 +1,29 @@
 #Дано вещественное число X и  целое число N (>0).
 #Найти значение выражения 1 - X^2/(2!) + X^4/(4!) - ..... + ((-1)^N - X^2N)/((2-N)!)/ N!
+try:
+    X = float(input("Введите значение X (вещественное число): "))
+    N = int(input("Введите значение N (целое число, N > 0): "))
 
-X = float(input("Введите значение X (вещественное число): "))
-N = int(input("Введите значение N (целое число, N > 0): "))
-s = 0
-sign = 1
-n = 0
-while n <= N:
-    factorial = 1
-    i = 1
-    while i <= 2 * n:
-        factorial *= i
-        i += 1
-    term = sign * (X ** (2 * n)) / factorial
-    s += term
-    sign *= -1
-    n += 1
-result = s + 1
+    if N <= 0:
+        print("Ошибка: N должно быть больше нуля")
+    else:
+        s = 0
+        sign = 1
+        n = 0
 
-print(f"Приближенное значение cos({X}): {result}")
+        while n <= N:
+            factorial = 1
+            i = 1
+            while i <= 2 * n:
+                factorial *= i
+                i += 1
+            term = sign * (X ** (2 * n)) / factorial
+            s += term
+            sign *= -1
+            n += 1
+            result = s + 1
+
+    print(f"Приближенное значение cos({X}): {result}")
+except ValueError:
+    print("Ошибка! Введите корректные числовые данные")
 
--- PZ_4/pz_4_2.py
+++ PZ_4/pz_4_2.py
@@ -1,12 +1,17 @@
 #Дано целое число N (>0)
 #Найти сумму 1^1 + 2^2 + N^N
 
-N = int(input("Введите целое число: "))
-total_sum = 0
-i = 1
+try:
+    N = int(input("Введите целое число: "))
+    total_sum = 0
+    i = 1
+    if N <= 0:
+        print("Ошибка: N должно быть больше нуля")
+    else:
+        while i <= N:
+            total_sum = total_sum + i ** i
+            i += 1
 
-while i <= N:
-    total_sum = total_sum + i ** i
-    i += 1
-
-print(total_sum)
+    print(total_sum)
+except ValueError:
+    print("Ошибка! Введите корректные числовые данные")
