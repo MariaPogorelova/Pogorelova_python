e6145d3daf40ce49d681dd4cb4c700b6656ac540
MariaPogorelova <masha@pogoreloff.ru>
2025-01-20 22:28:19 +0300

    ПЗ 7 сделала

--- PZ_7/pz_7_1.py
+++ PZ_7/pz_7_1.py
@@ -1,7 +1,20 @@
 #Дано целое положительное число. Вывести символы, изображающие цифры этого
 #числа (в порядке слева направо).
-num = input("Введите число: ")
-print(num)
-result = list(str(num))
+
+def check_number(value):
+    while True:
+        try:
+            num = int(input(value))
+            if num > 0:
+                return num
+            else:
+                print("Ошибка: Длина должна быть больше нуля.")
+        except ValueError:
+            print("Ошибка: Ввод должен быть числом.")
+
+number = check_number("Введите число: ")
+result = []
+for i in str(number):
+    result.append(int(i))
 print(result)
 
--- PZ_7/pz_7_2.py
+++ PZ_7/pz_7_2.py
@@ -3,15 +3,25 @@
 #строке, заменив в нем все последующие вхождения его первой буквы на символ «.»
 #(точка). Например, слово «МИНИМУМ» надо преобразовать в «МИНИ.У».
 #Количество пробелов между словами не изменять.
-string = "МИНИМУМ МИНИМУМ МИНИМУМ"
-result = 0
-i = 0
 
-while i < len(string):
-    if string[i] == " ":
-        result += " "
-        i += 1
-        continue
+string = "ОЛОВО ОЛОВО"
+words = string.split(' ')
 
-        start = i
-        first_char = string[i]
+def change_string(word):
+    if not word:
+        return word
+    first_letter = word[0]
+    check = first_letter
+    for char in word[1:]:
+        if char == first_letter:
+            check += '.'
+        else:
+            check += char
+    return check
+
+change_words = [change_string(word) for word in words]
+
+result = ' '.join(change_words)
+
+print(result)
+
