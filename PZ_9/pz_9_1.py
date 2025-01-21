f4b56102ed5f9259af29a5c3a6c75c6f4550d039
MariaPogorelova <masha@pogoreloff.ru>
2025-01-21 10:36:31 +0300

    пз 9 почти сделала

--- PZ_9/pz_9_1.py
+++ PZ_9/pz_9_1.py
@@ -2,11 +2,12 @@
 # предложении «Изучаем язык Питон». Вывести на экран каждую пару
 # «ключ:значение»
 
-S = "Изучаем язык Питон"
-L = S.split()
-U = set(L)
-M = {}
-for word in U:
-    M[word] = L.count(word)
-print(M)
-len(U)
+sentence = "Изучаем язык Питон Изучаем язык Питон"
+words = sentence.split() #list
+words_dict = {}
+for word in words:
+    words_dict[word] = words.count(word)
+for word, count in words_dict.items():
+    print(f"{word}: {count}")
+
+
