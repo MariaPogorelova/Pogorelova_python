6a60262d37a896ff34a13acebbba7fafe036d256
MariaPogorelova <masha@pogoreloff.ru>
2025-01-23 08:30:57 +0300

    ПЗ 9 сделала

--- PZ_9/pz_9_1.py
+++ PZ_9/pz_9_1.py
@@ -2,12 +2,21 @@
 # предложении «Изучаем язык Питон». Вывести на экран каждую пару
 # «ключ:значение»
 
-sentence = "Изучаем язык Питон Изучаем язык Питон"
+sentence = "Изучаем язык Питон"
 words = sentence.split() #list
 words_dict = {}
 for word in words:
     words_dict[word] = words.count(word)
 for word, count in words_dict.items():
     print(f"{word}: {count}")
+unique_words = list()
+for word in words:
+    if word in unique_words:
+        pass
+    else:
+        unique_words.append(word)
+print("Количество уникальных слов в предложении: ", len(unique_words))
+
+
 
 
