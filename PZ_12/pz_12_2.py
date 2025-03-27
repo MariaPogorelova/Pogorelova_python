cd4ad329e2848c29a558a25d6bf47a8d3786aafa
MariaPogorelova <masha@pogoreloff.ru>
2025-03-27 15:08:41 +0300

    ПЗ 12 выполнила

--- PZ_12/pz_12_2.py
+++ PZ_12/pz_12_2.py
@@ -1,5 +1,5 @@
-# 2.Из заданной строки отобразить только символы нижнего регистра. Использовать
-# библиотеку string. Строка'In PyCharm, you can specify third-party standalone applications and run them as External Tools'
+# 2.Из заданной строки отобразить только символы нижнего регистра.
+# Использовать библиотеку string. Строка'In PyCharm, you can specify third-party standalone applications and run them as External Tools'
 
 from string import ascii_lowercase
 
