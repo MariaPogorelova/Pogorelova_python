3069bd68d8259a2b312e99010cc321d167aa3f13
MariaPogorelova <masha@pogoreloff.ru>
2025-03-31 10:57:58 +0300

    ПЗ 13 выполнила

--- PZ_13/pz_13_2.py
+++ PZ_13/pz_13_2.py
@@ -5,3 +5,9 @@ matrix = [[i + j * size for i in range(size)] for j in range(size)]
 print("Исходная матрица:")
 for row in matrix:
     print(*row)
+
+min_num = min((min(_) for _ in matrix))
+print('Минимальное число: ', min_num)
+
+max_num = max((max(_) for _ in matrix))
+print('Максимальное число: ', max_num)
