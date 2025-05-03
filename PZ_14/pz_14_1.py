372eb8bd20d849ab13fb714b7cfb83eab3a525e5
MariaPogorelova <masha@pogoreloff.ru>
2025-05-03 13:49:41 +0300

    пз 14 сделала

--- PZ_14/pz_14_1.py
+++ PZ_14/pz_14_1.py
@@ -2,9 +2,16 @@
 # фамилий. Создать новый файл, в котором выполнить замену слова «роман» на слово
 # «произведение».
 import re
-old_word = re.compile("\bроман\b")
-new_word = 'произведение'
-with open('writer.txt', 'r', encoding='utf-8') as file:
+surname = r'^([\w\-]+)'
+with open('writer.txt','r', encoding='utf-8') as file:
     text = file.read()
-    print(re.sub(old_word, new_word, text))
+
+all_surnames = set(re.findall(surname, text, re.MULTILINE))
+count_surnames = len(all_surnames)
+print('Фамилии писателей: ', all_surnames)
+print('Количество фамилий: ', count_surnames)
+new_words = text.replace('роман', 'произведение')
+
+with open('new_writer.txt', 'w', encoding='utf-8') as new_file:
+    new_file.write(new_words)
 
