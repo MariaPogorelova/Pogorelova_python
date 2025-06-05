f57cea23b09f3ad2949a80fa8a4dbcb0a0835752
MariaPogorelova <masha@pogoreloff.ru>
2025-06-05 18:47:55 +0300

    ПЗ 17 сделала

--- PZ_17/pz_17_2.py
+++ PZ_17/pz_17_2.py
@@ -1,30 +1,40 @@
-import tkinter as tk
+#Проверить истинность высказывания: «Среди трех данных целых чисел есть хотя бы одна пара взаимно противоположных.
+from tkinter import *
 
-# Create the main window
-window = tk.Tk()
-window.title("PythonExamples.org")
-window.geometry("300x200")
+def find_count(event):
+    try:
+        n1 = int(num1.get())
+        n2 = int(num2.get())
+        n3 = int(num3.get())
+    except ValueError:
+        result['text'] = ('Ошибка! Введите корректные значения')
+        return
+    check_num = (n1 == -n2) or (n1 == -n3) or (n2 == -n3)
+    result['text'] = f"Среди трех данных \nцелых чисел \nесть хотя бы \nодна пара \nвзаимно противоположных? {check_num}"
 
-# Function to handle the Entry widget focus events
-def on_entry_focus_in(event):
-    if entry.get() == placeholder_text:
-        entry.delete(0, tk.END)
-        entry.configure(show="")
-        entry.configure(fg="black")
+root = Tk()
+root.title('Поиск взаимно противоположных чисел')
+root.geometry('500x200')
 
-def on_entry_focus_out(event):
-    if entry.get() == "":
-        entry.insert(0, placeholder_text)
-        entry.configure(show="*")
-        entry.configure(fg="gray")
+Label(text="Первое число").grid(row=1, column=0)
+num1 = Entry()
+num1.grid(row=1, column=1)
 
-# Create an Entry widget with placeholder text
-placeholder_text = "Enter your input.."
-entry = tk.Entry(window, fg="gray")
-entry.insert(0, placeholder_text)
-entry.bind("<FocusIn>", on_entry_focus_in)
-entry.bind("<FocusOut>", on_entry_focus_out)
-entry.pack()
 
-# Run the application
-window.mainloop()
+Label(text="Второе число").grid(row=2, column=0)
+num2 = Entry()
+num2.grid(row=2, column=1)
+
+Label(text="Третье число").grid(row=3, column=0)
+num3 = Entry()
+num3.grid(row=3, column=1)
+
+button1 = Button(text="Обработать")
+button1.grid(row=4, column=1)
+
+result = Label()
+result.grid(row=5, column=1)
+
+button1.bind('<Button-1>',find_count)
+
+root.mainloop()
